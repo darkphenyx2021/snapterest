11bce6b2d03efa162238efbc2a11466f7880b908
Joshua Fair <phenyx2021@gmail.com>
2016-07-07 16:56:50 -0400

    Working on stream of tweets. At shouldComponentUpdate method

--- source/app.js
+++ source/app.js
@@ -1,3 +1,8 @@
 /**
  * Created by joshua.fair on 7/6/2016.
  */
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Application = require('./components/Application.react');
+
+ReactDOM.render(<Application />, document.getElementById('react-application'));
